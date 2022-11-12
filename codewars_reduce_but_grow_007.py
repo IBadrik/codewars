05b006fd2e449bdba55f08a03a0bfb3a3a349097
Ilias Badretdinov <ilyasbadrik@mail.ru>
2022-11-12 18:13:02 +0300

    7th Kata

---
+++ codewars_reduce_but_grow_007.py
@@ -0,0 +1,7 @@
+def grow(arr):
+    result = 1
+    for i in arr:
+        result *= i
+    return result
+
+# Given a non-empty array of integers, return the result of multiplying the values together in order.
